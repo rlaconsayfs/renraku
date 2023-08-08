9c8481b754f5a8e84144a58a99b91547faa988fa
Raphael Laconsay <rlaconsay@fullscale.io>
2023-08-08 21:27:28 +0800

    Create Mapping Profiles and Utils

---
+++ backend/ContactManagerAPI/ContactManagerAPI.Utils/Mappings/ContactProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ContactManagerAPI.Entities.Dtos.ContactDtos;
+using ContactManagerAPI.Entities.Models;
+
+namespace ContactManagerAPI.Utils.Mappings
+{
+    public class ContactProfile : Profile
+    {
+        /// <summary>
+        /// Creates mapping profile for Contact entity
+        /// </summary>
+        public ContactProfile()
+        {
+            CreateMap<Contact, ContactDto>().ReverseMap();
+
+            CreateMap<Contact, ContactCreateDto>()
+                .ReverseMap()
+                .ForMember(contact => contact.PhoneNumbers, op => op.MapFrom(dto => dto.PhoneNumbers)) // Map phone numbers
+                .ForMember(contact => contact.FirstName, op => op.MapFrom(dto => dto.FirstName.ToTitleCase())) // Capitalize first letter of first name
+                .ForMember(contact => contact.LastName, op => op.MapFrom(dto => dto.LastName.ToTitleCase())) // Capitalize first letter of last name
+                .ForMember(contact => contact.DeliveryAddress, op => op.MapFrom(dto => dto.DeliveryAddress.ToTitleCase())) // Capitalize first letter of delivery address
+                .ForMember(contact => contact.BillingAddress, op => op.MapFrom(dto => dto.BillingAddress.ToTitleCase())); // Capitalize first letter of billing address
+
+            CreateMap<Contact, ContactUpdateDto>()
+                .ReverseMap()
+                .ForMember(contact => contact.FirstName, op => op.MapFrom(dto => dto.FirstName.ToTitleCase())) // Capitalize first letter of first name
+                .ForMember(contact => contact.LastName, op => op.MapFrom(dto => dto.LastName.ToTitleCase())) // Capitalize first letter of last name
+                .ForMember(contact => contact.DeliveryAddress, op => op.MapFrom(dto => dto.DeliveryAddress.ToTitleCase())) // Capitalize first letter of delivery address
+                .ForMember(contact => contact.BillingAddress, op => op.MapFrom(dto => dto.BillingAddress.ToTitleCase())); // Capitalize first letter of billing address
+        }
+    }
+}
---
+++ backend/ContactManagerAPI/ContactManagerAPI.Utils/Mappings/UserProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using ContactManagerAPI.Entities.Dtos.UserDtos;
+using ContactManagerAPI.Entities.Models;
+
+namespace ContactManagerAPI.Utils.Mappings
+{
+    public class UserProfile : Profile
+    {
+        /// <summary>
+        /// Creates a mapping profile for the User entity
+        /// </summary>
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+
+            CreateMap<User, UserCreateDto>()
+                .ReverseMap()
+                .ForMember(user => user.PasswordHash, op => op.MapFrom(dto => dto.Password))
+                .ForMember(user => user.FirstName, op => op.MapFrom(dto => dto.FirstName.ToTitleCase())) // Capitalize first letter of first name
+                .ForMember(user => user.LastName, op => op.MapFrom(dto => dto.LastName.ToTitleCase())); // Capitalize first letter of last name
+
+            CreateMap<User, UserUpdateDto>()
+                .ReverseMap()
+                .ForMember(user => user.PasswordHash, op => op.MapFrom(dto => dto.Password))
+                .ForMember(user => user.FirstName, op => op.MapFrom(dto => dto.FirstName.ToTitleCase())) // Capitalize first letter of first name
+                .ForMember(user => user.LastName, op => op.MapFrom(dto => dto.LastName.ToTitleCase())); // Capitalize first letter of last name
+        }
+    }
+}
