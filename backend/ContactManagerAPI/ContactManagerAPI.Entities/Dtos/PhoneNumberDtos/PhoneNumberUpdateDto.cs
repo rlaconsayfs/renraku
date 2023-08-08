2a1fec7a6c9bac47b29a7dbf86b961cff3e104e9
Raphael Laconsay <rlaconsay@fullscale.io>
2023-08-08 21:24:18 +0800

    Add Models and Dtos

---
+++ backend/ContactManagerAPI/ContactManagerAPI.Entities/Dtos/PhoneNumberDtos/PhoneNumberUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerAPI.Entities.Dtos.PhoneNumberDtos
+{
+    public class PhoneNumberUpdateDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string? Label { get; set; } = "Mobile";
+
+        [Required]
+        [MaxLength(15)]
+        public string ContactNumber { get; set; }
+    }
+}
---
+++ backend/ContactManagerAPI/ContactManagerAPI.Entities/Dtos/UserDtos/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerAPI.Entities.Dtos.UserDtos
+{
+    public class UserLoginDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Password { get; set; }
+    }
+}
